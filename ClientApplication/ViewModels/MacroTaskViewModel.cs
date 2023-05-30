bff12e61f5f40ba8d32bfc84844ca400381c429f
s201044 <s201044@th-ab.de>
2023-05-30 13:23:48 +0200

    update

---
+++ ClientApplication/ViewModels/MacroTaskViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using ClientApplication.Commands;
+using ClientApplication.Models;
+using ClientApplication.Utils;
+using Shared;
+
+namespace ClientApplication.ViewModels;
+
+public class MacroTaskViewModel: ViewModelBase
+{
+    public MacroTaskViewModel(INavigationService navigationService) : base(navigationService)
+    {
+        
+    }
+}
