ff676575573624702759afd64e48665486c570bd
snesse <snesse@SEBASTIANNE0E8B>
2023-06-04 18:03:03 +0200

    Add base for backTrackGame

--- ClientApplication/ViewModels/Games/BackTrackViewModel.cs
+++ ClientApplication/ViewModels/Games/BackTrackViewModel.cs
@@ -5,6 +5,16 @@ namespace ClientApplication.ViewModels.Games;
 
 public class BackTrackViewModel: AbstractGameViewModel
 {
+    private string _nextNumber = "6";
+    public string NextNumber
+    {
+        get => _nextNumber;
+        set
+        {
+            _nextNumber = value;
+            OnPropertyChanged(nameof(NextNumber));
+        }
+    }
     public BackTrackViewModel(INavigationService navigationService) : base(navigationService, GameType.BackTrack)
     {
       
--- ClientApplication/Views/Games/BackTrackView.xaml.cs
+++ ClientApplication/Views/Games/BackTrackView.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Windows.Controls;
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
 using ClientApplication.Utils;
 using ClientApplication.ViewModels.Games;
 
@@ -9,8 +13,39 @@ public partial class BackTrackView : UserControl
     public BackTrackView()
     {
         InitializeComponent();
-        InitializeComponent();
         var backTrackViewModel = new BackTrackViewModel(NavigationService.GetInstance());
         DataContext = backTrackViewModel;
+    }
+    
+    private void TextBox_PreviewKeyDown(object sender, TextCompositionEventArgs e)
+    {
+        // Überprüfen, ob die eingegebene Eingabe eine Zahl ist
+        if (!IsNumericInput(e.Text))
+        {
+            // Wenn es sich nicht um eine Zahl handelt, die Eingabe ignorieren
+            e.Handled = true;
+        }
+    }
+    
+    private bool IsNumericInput(string input)
+    {
+        // Überprüfen, ob die Eingabe eine Zahl ist
+        return int.TryParse(input, out _);
+    }
+    
+    private void textBox_Loaded(object sender, RoutedEventArgs e)
+    {
+        // Den Fokus auf die TextBox setzen
+        BackTrackTextBox.Focus();
+        // Die Eingabe deaktivieren
+        BackTrackTextBox.IsEnabled = false;
+        Task.Run(() =>
+        {
+            // Eine Wartezeit von 5 Sekunden einfügen
+            Task.Delay(TimeSpan.FromSeconds(5));
+            // Die Eingabe wieder aktivieren
+            BackTrackTextBox.IsEnabled = true;
+        });
+
     }
 }
